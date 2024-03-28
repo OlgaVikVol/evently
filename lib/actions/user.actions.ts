36a85c884850b1b86e54183cc5597f93630815fa
Olga Volobueva <olvik1605@gmail.com>
2024-03-28 19:46:54 -0400

    add db models, webhook

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,83 @@
+'use server'
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import Event from '@/lib/database/models/event.model'
+import { handleError } from "../utils";
+import { revalidatePath } from 'next/cache'
+import { WebhookEvent } from "@clerk/nextjs/server";
+import Order from "../database/models/order.model";
+
+let evt: WebhookEvent
+
+export async function createUser(user: CreateUserParams) {
+	try {
+		await connectToDatabase()
+
+		const newUser = await User.create(user)
+		return JSON.parse(JSON.stringify(newUser))
+	} catch (error) {
+		handleError(error)
+	}
+}
+
+export async function getUserById(userId: string) {
+	try {
+		await connectToDatabase()
+
+		const user = await User.findById(userId)
+
+		if(!user) throw new Error('User not found')
+		return JSON.parse(JSON.stringify(user))
+	} catch (error) {
+		handleError(error)
+	}
+}
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+	try {
+		await connectToDatabase()
+
+		const updatedUser = await User.findByIdAndUpdate({ clerkId }, user, { new: true })
+
+		if(!updateUser) throw new Error('User update failed')
+		return JSON.parse(JSON.stringify(updateUser))
+	} catch (error) {
+		handleError(error)
+	}
+}
+
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase()
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId })
+
+    if (!userToDelete) {
+      throw new Error('User not found')
+    }
+
+    // Unlink relationships
+    await Promise.all([
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+    ])
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+    revalidatePath('/')
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+  } catch (error) {
+    handleError(error)
+  }
+
+}
