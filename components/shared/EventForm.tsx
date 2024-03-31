778ebb4f7153cd3449de200ea0674398d9fa7460
Olga Volobueva <olvik1605@gmail.com>
2024-03-31 14:14:06 -0400

    add input, validator, update EventForm

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,6 +1,14 @@
 'use client'
 
 import { IEvent } from "@/lib/database/models/event.model";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator";
+import { eventDefaultValues } from "@/constants";
 
 type EventFormProps = {
 	userId: string;
@@ -11,11 +19,49 @@ type EventFormProps = {
 
 const EventForm = (props: EventFormProps) => {
 	const { type, event, eventId, userId } = props;
+	const initialValues = event && type === "Update" 
+	? {
+		...event,
+		startDateTime: new Date(event.startDateTime),
+		endDateTime: new Date(event.endDateTime)
+	} : eventDefaultValues
+
+	const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
 
 	return (
-		<div>
-			EventForm
-		</div>
+		<Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:flex-row">
+				<FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input placeholder="Event title..." {...field} className="input-field"/>
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+				</div>
+				
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
 	)
 }
 
